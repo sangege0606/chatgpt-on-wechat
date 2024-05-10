58b8590f413272ea59a0a71b1901589956b85afe
sangege0606 <sangege0606@outlook.com>
2024-05-10 09:01:33 +0800

    feat: 修改docker仓库为sangea0606

--- docker/build.latest.sh
+++ docker/build.latest.sh
@@ -4,7 +4,7 @@ unset KUBECONFIG
 
 # 构建一个Docker镜像。标签默认是`latest`。
 cd .. && docker build -f docker/Dockerfile.latest \
-             -t sangea/chatgpt-on-wechat .
+             -t sangea0606/chatgpt-on-wechat .
 
 # 给镜像添加一个新的标签。使用了shell命令替换$(date +%y%m%d)来动态生成标签，比如"240401"表示2024年4月1日）。
-docker tag sangea/chatgpt-on-wechat sangea/chatgpt-on-wechat:$(date +%y%m%d)
+docker tag sangea0606/chatgpt-on-wechat sangea0606/chatgpt-on-wechat:$(date +%y%m%d)
